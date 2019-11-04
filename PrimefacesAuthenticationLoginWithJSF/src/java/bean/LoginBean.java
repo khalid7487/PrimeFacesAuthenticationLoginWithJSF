c133a4eb27be7f1a8f05ae53ba3013b961ea1140
khalid7487 <khalid15-7487@diu.edu.bd>
2019-11-04 13:17:23 +0600

    faces config

--- PrimefacesAuthenticationLoginWithJSF/src/java/bean/LoginBean.java
+++ PrimefacesAuthenticationLoginWithJSF/src/java/bean/LoginBean.java
@@ -3,7 +3,14 @@ package bean;
 
 import dao.*;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
+@ManagedBean(name="loginBean")
+@SessionScoped
 public class LoginBean implements Serializable{
     private static final long serialVersionUID=1L;
     private String password;
@@ -36,7 +43,20 @@ public class LoginBean implements Serializable{
     public String loginProject(){
         boolean result=UserDAO.login(uname, password);
         if(result){
-            
+            HttpSession session=Util.getSession();
+            session.setAttribute("username", uname);
+            return "home";
+        }else{
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Invalid Login!",
+                            "Please Try Again!"));
+            return "login";
         }
+    }
+    public String logout(){
+        HttpSession session=Util.getSession();
+        session.invalidate();
+        return "login";
     }
 }
